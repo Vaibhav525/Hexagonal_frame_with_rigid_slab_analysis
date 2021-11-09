d411babca20670d46f79533fe6aac7009dbda5a7
Vaibhav525 <vaibhav.bihani525@gmail.com>
2021-11-09 10:41:49 +0530

    CVL759 Assignment_Timoshenko_beam_natural_frequency

---
+++ CVL759_Assignment_Timoshenko_beam_natural_freq/Cantilever.m
@@ -0,0 +1,75 @@
+%To calculate natural frequency of Timoshenko beam for Cantilever case
+%STEP 1: Input geometrical and physical properties of beam
+E=2.1e11;   %Pa
+G=8.1e10;   %Pa
+rho=7860;   %kg/m3
+L=1;        %m
+b=0.02;     %m
+h=0.08;     %m
+I=b*h^3/12; %m4
+A=b*h;      %m2
+k=1.2;      %Shear coefficient
+
+format long g   %For output formatting
+
+
+%STEP2: Applying Boundary conditions
+%For w<root(GkA/rho.I)
+%X(x)=P1 coshr1x + P2 sinhr1x + P3 cosr2x + P4 sinr2x
+%T(t)=ACoswt
+
+%For simply supported P1=P3=0
+%P2 and P4 are given by
+
+%Take series of w values
+w_l=[0:0.1:90000]';
+Res=zeros(900001,1);
+Det_prev=50;
+counter=1;
+for i=1:length(w_l)
+    w=w_l(i);   %Assumed w
+    %Calulate the parameters
+    a=w*w*rho/(k*G);
+    c=G*k*A/(E*I);
+    b=(rho*w*w/E)-c;
+    d=a+b+c;
+    e=a*b;
+
+    Delta= d*d-4*e;
+    %Calculate r1 and r2 for assumed w
+    if w<sqrt(k*G*A/(rho*I))
+        r1=sqrt(0.5*(-d+sqrt(Delta)));
+        r2=sqrt(0.5*(d+sqrt(Delta)));
+        A_matrix=[1 0 1 0;
+                  0 r1*(r1*r1+a+c) 0 r2*(-r2*r2+a+c);
+                  (r1*r1-d)*r1*sinh(r1*L) (r1*r1-d)*r1*cosh(r1*L) (r2*r2+d)*r2*sin(r2*L) (-r2*r2-d)*r2*cos(r2*L);
+                  (r1*r1+a)*cosh(r1*L) (r1*r1+a)*sinh(r1*L) (-r2*r2+a)*cos(r2*L) (-r2*r2+a)*sin(r1*L)];
+    else
+        r1=sqrt(0.5*(d-sqrt(Delta)));
+        r2=sqrt(0.5*(d+sqrt(Delta)));
+        A_matrix=[1 0 1 0;
+                  0 r1*(-r1*r1+a+c) 0 r2*(-r2*r2+a+c);
+                  (r1*r1+d)*r1*sin(r1*L) (-r1*r1-d)*r1*cos(r1*L) (r2*r2+d)*r2*sin(r2*L) (-r2*r2-d)*r2*cos(r2*L);
+                  (-r1*r1+a)*cos(r1*L) (-r1*r1+a)*sin(r1*L) (-r2*r2+a)*cos(r2*L) (-r2*r2+a)*sin(r1*L)];
+    end
+
+    %Calculate det(A_matrix)=0
+    Det=det(A_matrix);
+    Res(i)=Det; %Add the value for graph to results
+    if(Det*Det_prev<0 && counter<6)
+
+        %If Det(A_matrix) changes sign then this must be a root
+
+        disp(w);        %Display this natural frequency
+        counter=counter+1;  %Display only first 5 natural frequencies
+    end
+    Det_prev=Det;
+end
+
+%Plot shows variation of det(A) vs w, roots of this curve are natural
+%frequency
+
+plot(w_l,Res)
+xlabel("Frequecy(w),rad/s ------>")
+ylabel("Det(A) ------>")
+
